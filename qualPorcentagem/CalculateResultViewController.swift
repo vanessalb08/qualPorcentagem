400a57ab456cd96173bb55850d507d07dbe5605f
Vanessa <vanessalimab08@gmail.com>
2023-02-04 17:22:10 -0300

    create screens and connect then with navigation controller

---
+++ qualPorcentagem/CalculateResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CalculateResultViewController.swift
+//  qualPorcentagem
+//
+//  Created by Vanessa on 04/02/23.
+//
+
+import UIKit
+
+class CalculateResultViewController: UIViewController {
+
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- qualPorcentagem/ViewController.swift
+++ qualPorcentagem/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var initialValueTextField: UITextField!
+    @IBOutlet weak var finalValueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedCalculatedButton(_ sender: UIButton) {
+        let resultView: CalculateResultViewController? = UIStoryboard(name: "CalculateResultViewController", bundle: nil).instantiateViewController(withIdentifier: "CalculateResultViewController") as? CalculateResultViewController
+        self.present(resultView ?? UIViewController(), animated: true)
+    }
+    
 }
 
