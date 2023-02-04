023f04cd6aafd513a9e3395e1865b25fd46d5009
Vanessa <vanessalimab08@gmail.com>
2023-02-04 19:13:29 -0300

    add functions to calculate result

--- qualPorcentagem/CalculateResultViewController.swift
+++ qualPorcentagem/CalculateResultViewController.swift
@@ -12,10 +12,24 @@ class CalculateResultViewController: UIViewController {
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     
+    let value: Double
+    let result: String
+    
+    init?(coder: NSCoder, value: Double, result: String) {
+        self.value = value
+        self.result = result
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        valueLabel.text = String(value)
+        resultLabel.text = result
     }
     
 }
--- qualPorcentagem/ViewController.swift
+++ qualPorcentagem/ViewController.swift
@@ -14,11 +14,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // adicionar estilização aos textFields(teclado, etc)
+        // guard let para remover os unraped
+        // impedir a divisão por 0 - obrigar a informar os 2 campos para ativar o botão
+    }
+    
+    func calculatePercent() -> Double{
+        var initialValue: Double = 0
+        var finalValue :Double = 0
+        
+        initialValue = Double(initialValueTextField.text!) ?? 0
+        finalValue = Double(finalValueTextField.text!) ?? 0
+        
+        var calculate = ((finalValue - initialValue) / initialValue) * 100
+        return calculate
+    }
+    
+    func resultPercent() -> String {
+        let result = calculatePercent()
+        
+        if result >= 0{
+            return "acréscimo de:"
+        }
+        return "decréscimo de:"
     }
 
     @IBAction func tappedCalculatedButton(_ sender: UIButton) {
-        let resultView: CalculateResultViewController? = UIStoryboard(name: "CalculateResultViewController", bundle: nil).instantiateViewController(withIdentifier: "CalculateResultViewController") as? CalculateResultViewController
+        let resultView: CalculateResultViewController? = UIStoryboard(name: "CalculateResultViewController", bundle: nil).instantiateViewController(identifier: "CalculateResultViewController") {coder in return CalculateResultViewController(coder: coder, value: self.calculatePercent(), result: (self.resultPercent()))}
         self.present(resultView ?? UIViewController(), animated: true)
     }
     
